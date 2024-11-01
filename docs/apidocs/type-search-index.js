6ecf355f3ab501146bbe798ca3423f6f7cc9943e
Sallai András <andrewteki@gmail.com>
2024-11-01 15:33:05 +0100

    Refactored docs generator Bash script

--- docs/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"hu.szit","l":"Client"},{"p":"hu.szit","l":"Convert"}];updateSearchResults();
--- scripts/create_docs.sh
+++ scripts/create_docs.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 
+# Javadoc generálása
+# mvn javadoc:javadoc
 
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
-mvn javadoc:javadoc
+# Ellenőrzés, hogy sikerült-e a Javadoc generálása
+if [ -d "target/site/apidocs" ]; then
+    # Ha létezik a docs könyvtár, először töröljük, hogy friss verzió kerüljön bele
+    rm -rf docs
+    # Másolás a docs könyvtárba
+    cp -r target/site/apidocs docs
+    echo "A Javadoc sikeresen átmásolva a 'docs' könyvtárba."
+else
+    echo "Hiba: A Javadoc generálása nem sikerült vagy a 'target/site/apidocs' nem létezik."
+fi
 
 
